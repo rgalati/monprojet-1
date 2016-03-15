e7b38e52653197f46410c52e7a2d80903ba13796
Antoine de Choudens <antoine.dechoudens@etu.hesge.ch>
2016-03-15 09:37:06 +0100

    Premier commit

---
+++ src/projetjava/Coureur.java
@@ -0,0 +1,49 @@
+/**
+ * Tour du Canton de Genève
+ *
+ * Modélisation d'un coureur
+ *
+ * @author: Antoine de Choudens
+ *
+*/
+public class Coureur {
+
+  private String nom;       /* Nom du coureur */
+  private String prenom;    /* Prénom du coureur */
+  private boolean masculin; /* true si le sexe est masculin ("M"), false si le sexe est féminin ("F") */
+  private double temps;     /* Temps réalisé */
+
+  public Coureur (String nom, String prenom, char sexe, double temps) {
+    this.nom = nom;
+    this.prenom = prenom;
+    if(sexe == 'M'){
+        this.masculin = true;
+    }
+    else{
+        this.masculin = false;
+    }
+    this.temps = temps;
+  } // Constructeur
+
+  /** Retourne une représentation des informations du coureur sous la forme d'un String */
+  public String toString () {
+    if(masculin){
+        return "meilleur = "+nom+" "+prenom+" (M) "+temps;
+    }
+    else{
+        return "meilleur = "+nom+" "+prenom+" (F) "+temps;
+    }
+  } // toString
+  
+  public boolean equals(Object obj){
+      return true;
+  }
+  
+  public boolean getSexe(){
+      return this.masculin;
+  }
+  
+  public double getTemps(){
+      return this.temps;
+  }
+} // Coureur
---
+++ src/projetjava/FileToStr.java
@@ -0,0 +1,31 @@
+import java.io.*;
+/**
+ * Lecture d'un fichier texte dans un String
+ *
+ * @author Peter DAEHNE - HEG-Genève
+ * @version Version 1.0 - 28.09.2006
+*/
+public class FileToStr {
+
+  private static final int EOF = -1;
+
+  /** Lecture d'un fichier texte dans un String.
+      @param fileName Le nom du fichier texte.
+      @return le String contenant l'ensemble des caractères du fichier lu. */
+  public static String read (String fileName) {
+    try {
+      FileInputStream f = new FileInputStream(fileName);
+      StringBuffer b = new StringBuffer(f.available());
+      int c = f.read();
+      while (c != EOF) {
+        b.append((char)c);
+        c = f.read();
+      }
+      f.close();
+      return b.toString();
+    }
+    catch (FileNotFoundException e0) {e0.printStackTrace(); return "";}
+    catch (IOException e1) {e1.printStackTrace(); return "";}
+  } // read
+
+} // FileToStr
